051ddbf7b330a0af9e0811afda539fe0ff239393
Becky Smith <rebkwok@gmail.com>
2014-10-26 14:56:58 +0000

    gallery urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'gallery.views',
-    url(r'^gallery',         'gallery', name='gallery'),
+    url(r'^',         'gallery', name='gallery'),
+    url(r'^(?P<category_id>\d+)/category/$', 'gallery_category', name='gallery_category'),
 )
